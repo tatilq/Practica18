17f639bf8df00c29a1437dcd496f63fabff95a43
Tatiana Limache Quispe <tati34che@gmail.com>
2016-12-29 17:02:52 -0500

    Modificacion adicional

--- Casi_Palindrome/main.js
+++ Casi_Palindrome/main.js
@@ -1,27 +1,16 @@
 function casiPalindrome(palabra) {
-    var operacion="";
-    var n=0;
-    for(var c=0; c<palabra.length; c++)
-    {                   
-            if(palabra[c]!=0)
-            {
-                operacion[c]=palabra[c];
-                n=n+1;                          
-            }
-            else
-                {
-                    return false;
-                }         
-    }
-    for(var i=0; i<n; i++){
-        for(var a=n-1; a>=0; a--){
-            if(operacion[i]!=operacion[a]){
-                return false;
-            }
-            else
-                {
-                    return true;
-                }
-        }
-    }
+    var tam = palabra.length;
+    var palindrome;
+    var conta = 0;
+     for (i=0, j=tam-1; i<tam/2, j>tam/2; i++, j--){
+         if(palabra[i] !=palabra[j]){
+             conta++;
+         }
+      }
+         if(conta<=2){
+            palindrome=true;
+         }else{
+             palindrome=false;
+         }
+     return palindrome;
 }
